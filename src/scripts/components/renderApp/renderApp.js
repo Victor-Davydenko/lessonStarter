9d6a45f49ea3ca7e1363b4ecfc2b9a81fa2d954e
Victor-Davydenko <59482820+Victor-Davydenko@users.noreply.github.com>
2021-02-02 10:12:27 +0200

    functionality added

--- src/scripts/components/renderApp/renderApp.js
+++ src/scripts/components/renderApp/renderApp.js
@@ -2,10 +2,31 @@ import renderBar from "../renderBar";
 import renderCalendar from "../renderCalendar";
 
 const renderApp = () => {
-  const appElement = document.getElementById("appRoot");
-  const currentDate = new Date();
-  renderCalendar({ appElement, currentDate });
-  renderBar({ appElement, currentDate });
+    const appElement = document.getElementById("appRoot");
+    let currentDate = new Date();
+    renderBar({appElement, currentDate});
+    renderCalendar({appElement, currentDate});
+    const previousButton = document.createElement('button');
+    const nextButton = document.createElement('button');
+    previousButton.innerText = 'Previous';
+    nextButton.innerText = 'Next';
+    previousButton.setAttribute('id', 'prev');
+    nextButton.setAttribute('id', 'next');
+    appElement.prepend(nextButton);
+    appElement.prepend(previousButton);
+    previousButton.onclick = () => {
+        document.querySelector('table').innerHTML = '';
+        currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1);
+        renderBar({appElement, currentDate});
+        renderCalendar({appElement, currentDate});
+    };
+    nextButton.onclick = () => {
+        document.querySelector('table').innerHTML = '';
+        currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1);
+        renderBar({appElement, currentDate});
+        renderCalendar({appElement, currentDate});
+    };
+
 };
 
 export default renderApp;
--- src/scripts/components/renderCalendar/renderCalendar.js
+++ src/scripts/components/renderCalendar/renderCalendar.js
@@ -1,11 +1,30 @@
-const renderCalendar = ({ appElement, currentDate }) => {
-  const calendarContainer = document.createElement("table");
-  const calendarHead = document.createElement("thead");
-  const calendarBody = document.createElement("tbody");
-  calendarContainer.prepend(calendarHead); // This element must contain tr > th*monthLength > <span>DayName</span> + <span>DayNum</span>
-  calendarContainer.append(calendarBody); // This element must contain tr > td*monthLength
-  appElement.append(calendarContainer);
-  // let currentDate = new Date();
+const renderCalendar = ({appElement, currentDate}) => {
+    let calendarContainer;
+    const table = document.querySelector('.table');
+    table ? calendarContainer = table : calendarContainer = document.createElement("table");
+    calendarContainer.classList.add('table');
+    const calendarHead = document.createElement("thead");
+    const calendarBody = document.createElement("tbody");
+    calendarContainer.prepend(calendarHead); // This element must contain tr > th*monthLength > <span>DayName</span> + <span>DayNum</span>
+    calendarContainer.append(calendarBody); // This element must contain tr > td*monthLength
+    appElement.append(calendarContainer);
+
+    let currentMonthLastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+    let tableHeadString = '';
+    let dayOfWeekEl = '';
+    let date = '';
+    for (let i = 1; i <= currentMonthLastDay; i++) {
+        let dayOfWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), i).toLocaleDateString('en', {weekday: 'short'}).substr(0, 2);
+        if (dayOfWeek.includes('Sa') || dayOfWeek.includes('Su')) {
+            dayOfWeekEl = `<span class="weekdayHolder weekends">${new Date(currentDate.getFullYear(), currentDate.getMonth(), i).toLocaleDateString('en', {weekday: 'short'}).substr(0, 2)}</span>`;
+            date = `<span class="dateHolder weekends">${new Date(currentDate.getFullYear(), currentDate.getMonth(), i).toLocaleDateString('en', {day: 'numeric'}).substr(0, 2)}</span>`;
+        } else {
+            dayOfWeekEl = `<span class="weekdayHolder">${new Date(currentDate.getFullYear(), currentDate.getMonth(), i).toLocaleDateString('en', {weekday: 'short'}).substr(0, 2)}</span>`;
+            date = `<span class="dateHolder">${new Date(currentDate.getFullYear(), currentDate.getMonth(), i).toLocaleDateString('en', {day: 'numeric'}).substr(0, 2)}</span>`;
+        }
+        tableHeadString += `<th>${dayOfWeekEl}${date}</th>`;
+    }
+    calendarHead.innerHTML = `<tr>${tableHeadString}</tr>`;
 };
 
 export default renderCalendar;
